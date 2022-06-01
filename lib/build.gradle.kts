8b1bd2f654c192f7d98c617dd45b67f64ea393ea
Aeden Jameson <aeden.jameson@gmail.com>
2022-05-31 23:37:06 -0700

    update gradle & remove .idea/ files from gitJ

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
     idea
 
     // Apply the org.jetbrains.kotlin.jvm Plugin to add support for Kotlin.
-    id("org.jetbrains.kotlin.jvm") version "1.6.10"
+    id("org.jetbrains.kotlin.jvm") version "1.6.21"
 
     // https://plugins.gradle.org/plugin/com.adarshr.test-logger
     id("com.adarshr.test-logger") version "3.0.0"
